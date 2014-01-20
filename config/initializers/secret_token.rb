56d2175f0d9beb796fade013557e9ef004a6d1c5
Ariel1990 <ingeniero2@opensky.com.co>
2014-01-20 11:44:13 -0500

    Repositorio inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PrimeraApp::Application.config.secret_key_base = 'aa156efa1a329c36183db7990276b1a270964b510a85936c4f180c408b68aeb7e03e0b40385808eea49b108e7f0df054a51c2007d02e6ad1515544d05a373227'
